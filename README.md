3053081b08b6e17735a528251379d6da698b9b07
lottid <1550466442@qq.com>
2017-11-28 16:22:35 +0800

    建立一个组，移动代码过来

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # muti-pro
+
+> 基于Vue的多页应用
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# 增加一个tmpl生成，pages下目录结构，从tmpls获取(默认new-tmpl)
+npm run tmpl yourdir
+
+```
+## 文件说明
+*  pro.config.json----本地预览地址及反向代理设置
+*  src/stores ---- 存放公用vuex模块，并增加了命名空间
+``` bash
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
+```
+
---
+++ src/components/login/login.vue
@@ -0,0 +1,74 @@
+<template>
+<div>
+<div class="login" v-if="!isLogin">
+<p>
+<input type="text" name="" v-model="loginName" >
+</p>
+<p>
+<input type="password" name="" v-model="passWord">
+</p>
+<p>
+<button @click="login()">login</button>
+</p>
+</div>
+<div class="main" v-else>
+<p>
+<a href="./report.html" target="_blank">report</a>
+</p>
+<p>
+<a href="./exam.html" target="_blank">exam</a>
+</p>
+<p>
+<a href="./counter.html" target="_blank">counter</a>
+</p>
+<p>
+<button @click="loginOut()">loginOut</button>
+</p>
+</div>
+</div>
+</template>
+
+<script>
+import "@/assets/normalize.css";
+export default {
+  name: "login",
+  data() {
+    return {
+      loginName: "",
+      passWord: "",
+      isLogin: this.$store.getters["loginStore/isLogin"]
+    };
+  },
+  methods: {
+    login() {
+      if (this.loginName && this.passWord) {
+        this.$store
+          .dispatch("loginStore/loginAction", {
+            loginName: this.loginName,
+            passWord: this.passWord
+          })
+          .then(res => {
+            this.isLogin = this.$store.getters["loginStore/isLogin"];
+          });
+      } else {
+        console.log("error");
+      }
+    },
+    loginOut() {
+      this.$store.dispatch("loginStore/loginOutAction").then(res => {
+        this.resetFrom();
+        this.isLogin = this.$store.getters["loginStore/isLogin"];
+      });
+    },
+    resetFrom() {
+      this.loginName = "";
+      this.passWord = "";
+    }
+  }
+};
+</script>
+<style  scoped>
+p {
+  text-align: center;
+}
+</style>
